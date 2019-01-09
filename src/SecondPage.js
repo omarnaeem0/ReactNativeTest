8bfe4cfed3b50199270f6c54d32025b76c57bf45
omarnaeem0 <omar_naeem@live.com>
2019-01-09 12:35:08 +0500

    removed unnecessary stuff

--- src/SecondPage.js
+++ src/SecondPage.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class HomePage extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Omar's App!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
         <Text style={styles.instructions}>This is the SecondPage</Text>
         <Button 
           onPress={() => this.props.navigation.goBack()}
@@ -31,11 +21,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
   instructions: {
     textAlign: 'center',
